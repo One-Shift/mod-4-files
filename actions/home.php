12a1eb2b3dfbf87df61c7985cd8ece5a2e131416
Rafael Duarte <rafaeljfduarte@gmail.com>
2018-08-21 18:19:06 +0100

    Files list Simplified

--- actions/home.php
+++ actions/home.php
@@ -6,29 +6,18 @@ $file = new file();
 $files = $file->returnFiles("TRUE");
 
 if(!empty($files)) {
-	if(count($files) > 1) {
-		foreach ($files as $index => $file) {
-			if (!isset($list)) {
-				$list = "";
-			}
-
-			$list .= bo3::c2r([
-				"id" => $file->id,
-				"file" => $file->file,
-				"module" => $file->module,
-				"id-ass" => $file->id_ass,
-				"sort" => $file->sort,
-				"date-update" => $file->date_update,
-			], $item_tpl);
+	foreach ($files as $index => $file) {
+		if (!isset($list)) {
+			$list = "";
 		}
-	} else {
-		$list = bo3::c2r([
-			"id" => $files->id,
-			"file" => $files->file,
-			"module" => $files->module,
-			"id-ass" => $files->id_ass,
-			"sort" => $files->sort,
-			"date-update" => $files->date_update,
+
+		$list .= bo3::c2r([
+			"id" => $file->id,
+			"file" => $file->file,
+			"module" => $file->module,
+			"id-ass" => $file->id_ass,
+			"sort" => $file->sort,
+			"date-update" => $file->date_update,
 		], $item_tpl);
 	}
 }
